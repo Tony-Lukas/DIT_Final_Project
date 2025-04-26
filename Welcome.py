1aaca617dd92fdca51c065ea6134cf84f0babe9a
AsiaJuice199 <phonepyaekyaww@gmail.com>
2025-04-26 18:12:42 +0700

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -3,5 +3,5 @@ from main import MyClass
 if __name__ == '__main__':
     print("Welcome to Jurassic Park Research Faculty (J.P.R.F)!")
     print("How can we help you today?")
-    Dinosaurs = Dino('Dinosaurs')
+    Dinosaurs = MyClass('Dinosaurs')
     Dinosaurs.run()
